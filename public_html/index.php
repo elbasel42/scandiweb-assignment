e16d31ddadaa7a9ba7c4cce0470cb98ee185d9d5
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2023-11-15 11:47:11 +0200

    fix syntax error

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
-<?php
+https://crud-scandiweb-42.000webhostapp.com/<?php
 $file = fopen('test.txt', 'w');
 fwrite($file, 'hello world');
 rewind($file);
-$writtenToFile = fread($file, filesize('./test.txt');
+$writtenToFile = fread($file, filesize('./test.txt'));
 echo $writtenToFile;
