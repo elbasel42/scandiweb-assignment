c797e9b820cbd2030293138372706008937014af
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2023-12-03 09:51:43 +0200

    delete old dir

--- public_html/old/index.php
+++
@@ -1,6 +0,0 @@
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="file-upload" id="file-upload">
-    <input type="submit" value="submit">
-</form>
-
-
